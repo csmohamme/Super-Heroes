7833e57b8c2ecdc772c804aae4b4b028bffbaf91
Mohammed Almostafa <58958280+csmohamme@users.noreply.github.com>
2022-11-09 00:02:26 -0800

    superHero Model & Controller

---
+++ Controllers/SuperHeroController.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Super_Heroes.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SuperHeroController : ControllerBase
+    {
+        
+    }
+}
